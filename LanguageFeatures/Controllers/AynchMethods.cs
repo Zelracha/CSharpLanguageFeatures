d9054d9aeff52b2fa4dfe267b006712f3821f75c
riotl <riotl@DESKTOP-V70I17V>
2024-05-24 16:52:31 +0800

    Added AsynchMethods
    ERROR NOT FIXED: INTERFACE

---
+++ LanguageFeatures/Controllers/AynchMethods.cs
@@ -0,0 +1,15 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LanguageFeatures.Controllers
+{
+    public class AynchMethods : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] { $"Page Length: {length}" });
+        }
+    }
+}
